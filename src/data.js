950ddab769eb40d740574c3e621921c8a1d745ae
Andrey Dovganyuk <andrewdovg@gmail.com>
2020-06-12 10:52:26 +0300

    Ready to deploy

--- src/data.js
+++ src/data.js
@@ -8,8 +8,8 @@ export const placeInfo = [
     title: "Molestias officia excepturi",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates qui voluptate odio quod doloremque unde reprehenderit velit quaerat. Deserunt voluptatem natus suscipit aliquid, eligendi sequi dicta porro? Laudantium, libero est?",
-    // img: '.././images/garaj.jpg',
-    img: "https://8020coding.github.io/images/garaj.jpg",
+    img: ".././images/garaj.jpg",
+
     maps:
       "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1123.3850145412064!2d37.600589158335865!3d55.72774864513666!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46b54b0be3ac9d11%3A0x735806d25b3bdc55!2z0JzRg9C30LXQuSDRgdC-0LLRgNC10LzQtdC90L3QvtCz0L4g0LjRgdC60YPRgdGB0YLQstCwIMKr0JPQsNGA0LDQtsK7!5e0!3m2!1sru!2sru!4v1547763408711",
   },
@@ -22,7 +22,7 @@ export const placeInfo = [
     title: "Lorem unitye",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Est incidunt quo veritatis ad, nobis dolorum corrupti excepturi, natus fugit necessitatibus nihil nam minus explicabo debitis quod accusamus consectetur a eaque.",
-    img: "https://8020coding.github.io/images/mamm.jpg",
+    img: ".././images/mamm.jpg",
     maps:
       "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1530.8494832689219!2d37.59693065124095!3d55.74157162835216!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46b54baea59877dd%3A0xb4d0c3eb557a1c98!2zwqvQnNCe0KHQmtCe0JLQodCa0JjQmSDQlNCe0Jwg0KTQntCi0J7Qk9Cg0JDQpNCY0JjCuw!5e0!3m2!1sru!2sru!4v1547845790316",
   },
@@ -36,7 +36,7 @@ export const placeInfo = [
       "elit. Fugit, provident corrupti repudiandae nihil harum vel? Ducimus quos inventore suscipit sed laudantium eum quae, ex obcaecati quisquam? Pariatur omnis in reiciendis!",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium labore modi error sed ad quis obcaecati aliquid officia rerum alias quia voluptatum, consequatur at optio quas nisi autem necessitatibus nemo!",
-    img: "https://8020coding.github.io/images/pushkin.jpg",
+    img: ".././images/pushkin.jpg",
     maps:
       "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d17967.866965087615!2d37.60335432286895!3d55.74141791432578!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb9384e1a3b2da434!2z0JPQvtGB0YPQtNCw0YDRgdGC0LLQtdC90L3Ri9C5INC80YPQt9C10Lkg0JAu0KEuINCf0YPRiNC60LjQvdCw!5e0!3m2!1sru!2sru!4v1547878731704",
   },
@@ -48,7 +48,7 @@ export const placeInfo = [
     title: "Perspiciatis illum",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda quibusdam nostrum dicta vitae, illo numquam autem excepturi rem unde eos exercitationem perferendis expedita, blanditiis eaque facilis explicabo aliquid doloribus rerum?",
-    img: "https://8020coding.github.io/images/tretyakovka.jpg",
+    img: ".././images/tretyakovka.jpg",
     maps:
       "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2245.9848620916428!2d37.618675215617316!3d55.741392000339864!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46b54afe32078357%3A0xa50d6e8b9a7b028f!2z0JPQvtGB0YPQtNCw0YDRgdGC0LLQtdC90L3QsNGPINCi0YDQtdGC0YzRj9C60L7QstGB0LrQsNGPINCz0LDQu9C10YDQtdGP!5e0!3m2!1sru!2sru!4v1547878659681",
   },
@@ -60,7 +60,7 @@ export const placeInfo = [
     title: "Consequuntur quo",
     description:
       "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda illo numquam commodi nihil nam odit, minima perferendis delectus, quibusdam modi recusandae suscipit nobis cumque, veniam similique voluptas totam quasi mollitia.",
-    img: "https://8020coding.github.io/images/dom-fotografii.jpg",
+    img: ".././images/dom-fotografii.jpg",
     maps:
       "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2245.9763498372804!2d37.59654121561725!3d55.741539900328505!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46b54baea59877dd%3A0xb4d0c3eb557a1c98!2zwqvQnNCe0KHQmtCe0JLQodCa0JjQmSDQlNCe0Jwg0KTQntCi0J7Qk9Cg0JDQpNCY0JjCuw!5e0!3m2!1sru!2sru!4v1547878587877",
   },
@@ -73,7 +73,7 @@ export const placeInfo = [
     title: "Animi nobis",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident, nostrum. Non, cupiditate facere quidem quibusdam atque aliquam corrupti minus esse dolor doloremque, vel aut. Quae eos quo consequuntur id commodi?",
-    img: "https://8020coding.github.io/images/mars.jpg",
+    img: ".././images/mars.jpg",
     maps:
       "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2244.397087990157!2d37.62463341561838!3d55.76897499821247!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46b54a68e2e342b7%3A0x934bc1e9948c826b!2z0KbQtdC90YLRgCDQnNCw0YDRgQ!5e0!3m2!1sru!2sru!4v1547878491776",
   },
@@ -83,35 +83,35 @@ export const reviews = [
   {
     id: 1,
     name: "John",
-    avatar: "https://8020coding.github.io/avatars/avatar-1.png",
+    avatar: ".././avatars/avatar-1.png",
     comment:
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus, quaerat repellendus. Ad at sit laboriosam sint ea mollitia incidunt voluptatibus inventore libero dolorem velit dolor nobis odit tempora, ex consequuntur.",
   },
   {
     id: 2,
     name: "Mike",
-    avatar: "https://8020coding.github.io/avatars/avatar-2.png",
+    avatar: ".././avatars/avatar-2.png",
     comment:
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus, quaerat repellendus. Ad at sit laboriosam sint ea mollitia incidunt voluptatibus inventore libero dolorem velit dolor nobis odit tempora, ex consequuntur.",
   },
   {
     id: 3,
     name: "Sofia",
-    avatar: "https://8020coding.github.io/avatars/avatar-3.png",
+    avatar: ".././avatars/avatar-3.png",
     comment:
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus, quaerat repellendus. Ad at sit laboriosam sint ea mollitia incidunt voluptatibus inventore libero dolorem velit dolor nobis odit tempora, ex consequuntur.",
   },
   {
     id: 4,
     name: "Joseph",
-    avatar: "https://8020coding.github.io/avatars/avatar-4.png",
+    avatar: ".././avatars/avatar-4.png",
     comment:
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus, quaerat repellendus. Ad at sit laboriosam sint ea mollitia incidunt voluptatibus inventore libero dolorem velit dolor nobis odit tempora, ex consequuntur.",
   },
   {
     id: 5,
     name: "Natasha",
-    avatar: "https://8020coding.github.io/avatars/avatar-5.png",
+    avatar: ".././avatars/avatar-5.png",
     comment:
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus, quaerat repellendus. Ad at sit laboriosam sint ea mollitia incidunt voluptatibus inventore libero dolorem velit dolor nobis odit tempora, ex consequuntur.",
   },
@@ -126,7 +126,7 @@ export const detailInfo = {
   title: "Molestias officia excepturi",
   description:
     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates qui voluptate odio quod doloremque unde reprehenderit velit quaerat. Deserunt voluptatem natus suscipit aliquid, eligendi sequi dicta porro? Laudantium, libero est?",
-  img: "https://8020coding.github.io/images/garaj.jpg",
+  img: ".././images/garaj.jpg",
   maps:
     "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1123.3850145412064!2d37.600589158335865!3d55.72774864513666!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46b54b0be3ac9d11%3A0x735806d25b3bdc55!2z0JzRg9C30LXQuSDRgdC-0LLRgNC10LzQtdC90L3QvtCz0L4g0LjRgdC60YPRgdGB0YLQstCwIMKr0JPQsNGA0LDQtsK7!5e0!3m2!1sru!2sru!4v1547763408711",
 };
